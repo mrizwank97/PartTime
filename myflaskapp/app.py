a46bcb206255ee8484cde2a177cd4e5a1eb87c86
nustian16 <mkhalid.bscs16seecs@seecs.edu.pk>
2019-04-25 02:15:17 +0500

    Wep app in flash for dengue outbreak

---
+++ myflaskapp/app.py
@@ -0,0 +1,173 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+from werkzeug import secure_filename
+import pickle
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
+
+app = Flask(__name__)
+    
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
+
+#Articles = Articles()
+
+# Index
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# About
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('file_upload'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/file_upload')
+@is_logged_in
+def file_upload():
+    return render_template('file_upload.html')
+
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      fig = create_figure(f.filename)
+      output = io.BytesIO()
+      FigureCanvas(fig).print_png(output)
+      return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure(file):
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    features = pd.read_csv(file)
+    for i in features.columns:
+        df = features[i]
+        mean = df[df != 0].mean()
+        df[df == 0] = mean
+    loaded_model = pickle.load(open("dengue-weights.dat", "rb"))
+    y_pred = loaded_model.predict(features)
+    predictions = [round(value) for value in y_pred]
+    axis.plot(np.exp(predictions))
+    return fig
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
